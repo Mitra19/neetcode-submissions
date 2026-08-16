ea84d7af08b43b03f0176ae0c714750f586171b2
Jaydeb Mitra <38115182+Mitra19@users.noreply.github.com>
2026-08-16 14:06:07 +0530

    Add: strategy - submission-0

---
+++ Design Patterns/strategy/submission-0.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+class Person:
+    def __init__(self, lastName: str, age: int, married: bool):
+        self.lastName = lastName
+        self.age = age
+        self.married = married
+
+    def getLastName(self) -> str:
+        return self.lastName
+
+    def getAge(self) -> int:
+        return self.age
+
+    def isMarried(self) -> bool:
+        return self.married
+
+class PersonFilter(ABC):
+    @abstractmethod
+    def apply(self, person: Person) -> bool:
+        pass
+
+class AdultFilter(PersonFilter):
+    def apply(self, person: Person) -> bool:
+        return person.getAge() > 17
+class SeniorFilter(PersonFilter):
+    def apply(self, person: Person) -> bool:
+        return person.getAge() > 64
+
+class MarriedFilter(PersonFilter):
+    def apply(self, person: Person) -> bool:
+        return person.isMarried()
+
+class PeopleCounter:
+    def __init__(self):
+        self.filter: PersonFilter = None
+
+    def setFilter(self, filter: PersonFilter) -> None:
+        self.filter = filter
+
+    def count(self, people: List[Person]) -> int:
+        ans = []
+        for person in people:
+            if self.filter.apply(person):
+                ans.append(person)
+        return len(ans)
+    
